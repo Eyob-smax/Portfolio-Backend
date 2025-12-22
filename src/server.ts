1dc0d9fe1606c5bf0d1ce1b8cb54aa5830cbf57d
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-12-22 15:10:42 +0300

    feat:addin cors policy

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["eyobsimachew.verve.app"],
+    origin: ["https://eyobsimachew.vercel.app/"],
   })
 );
 const port = process.env["PORT"] || 3000;

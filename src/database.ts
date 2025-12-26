3c6531acea58523cacb0cbc47df579cb79c1cd69
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-12-26 10:06:31 +0300

    adding and endpoint for the contact meassage

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,10 @@
-// src/database/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error", "query", "warn"], // optional
-  });
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ["error"] });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
